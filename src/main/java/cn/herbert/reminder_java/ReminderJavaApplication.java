fc9a88c80b2256ebfb5edaf60d6fa3ffc8d90df7
Herbert_Hyb <he.yibin@outlook.com>
2025-05-17 20:38:19 +0200

    init

---
+++ src/main/java/cn/herbert/reminder_java/ReminderJavaApplication.java
@@ -0,0 +1,13 @@
+package cn.herbert.reminder_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReminderJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/herbert/reminder_java/ReminderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.herbert.reminder_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
